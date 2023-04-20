6516045173f264b097087556974fba8cd2a09a48
Faraz <111528872+Faraz-Ghani@users.noreply.github.com>
2023-04-20 20:28:36 +0500

    Added Points Object

--- GunMan/Assets/Scripts/Other/FruitController.cs
+++ GunMan/Assets/Scripts/Other/FruitController.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class FruitController : MonoBehaviour
 {
-    public float speed;
     public Generator generator;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +14,15 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          transform.Translate(Vector2.down * speed * Time.deltaTime);
-   }
-
+        
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
-            Debug.Log("Game Over");
+            Debug.Log("Point++");
         }
         else if(other.tag =="Ground"){
             generator.Generate();
             Destroy(gameObject);    
-        }
-        else if(other.tag == "Bullet"){
-            Debug.Log("Hit");
-            Destroy(gameObject);
         }
     }
 }
