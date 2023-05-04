2f49fefcd735947df6ff1c2913655e7bb0e04578
Faraz <111528872+Faraz-Ghani@users.noreply.github.com>
2023-05-04 12:28:20 +0500

    Added Eating Logic

--- SnakeGame (GHW '23)/Assets/Scripts/Snake_Movement.cs
+++ SnakeGame (GHW '23)/Assets/Scripts/Snake_Movement.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Snake_Movement : MonoBehaviour
 {
+    private List<Transform> _segments ; 
+    public int score=0;
     public float timer = 0;
     public float timerMax = 1;
     public Vector2 GridPosition = new Vector2(0,0);
     // Start is called before the first frame update
     void Start()
     {
-        
+      _segments = new List<Transform>();
+      _segments.Add(this.transform);   
     }
 
     // Update is called once per frame
     void Update()
     {
+    //handle input
      if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             GridPosition.y += .5f;
@@ -33,11 +37,22 @@ public class Snake_Movement : MonoBehaviour
             GridPosition.x += .5f;
         } 
  
+    
+    //handle movement
     if(GridPosition.x > 8 || GridPosition.x < -8 || GridPosition.y > 4.5 || GridPosition.y < -4.5){
     Debug.Log("Game Over");
     }
     else{
     transform.position = new Vector3(GridPosition.x, GridPosition.y, 0);    
-        }
+    }
+
+}
+
+private void OnTriggerEnter2D(Collider2D other) {
+    if(other.gameObject.tag == "Food"){
+        Destroy(other.gameObject);
+        Debug.Log("Food Eaten");
+        score++;
+    }
 }
 }
